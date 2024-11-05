e66ae949559ca3902e00faa4b5f36df976bd2f13
Asif Ali <jamaliasif637@gmail.com>
2024-11-05 20:00:50 +0500

    Add files via upload

---
+++ 3.Blood_donation.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int age, weight;
+	
+	printf("Enter the Age :");
+	scanf("%d",&age);
+	
+	printf("Enter the Weight :");
+	scanf("%d",&weight);
+	
+	if(age >= 18 &&  age <=65){
+		if(weight >=50){
+			printf("Eligible for blood donation .");
+		}
+	}
+	else {
+		printf("Not Eligible for blood donation .");
+	}
+	
+	
+	getch();
+}
